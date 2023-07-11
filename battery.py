456700f4a3531f75392a152a25bcb3c0a8bbc2ec
sumanthpagadala <pagadala.sumanth2002@gmail.com>
2023-07-11 22:13:20 +0530

    updated code

--- battery.py
+++ battery.py
@@ -1,11 +1,21 @@
 import psutil
+from plyer import notification
+from time import sleep
 
-battery = psutil.sensors_battery()
-percentage = battery.percent
+MIN_BATTERY_THRESHOLD = 20
+MAX_CHARGE_THRESHOLD = 95
+FULL_CHARGE = 100
 
-if percentage > 95:
-    print("The charging is about to be full")
-elif percentage == 100:
-    print("Charging full, time to unplug")
-else:
-    print(f"Battery percentage: {percentage}%")
+while True:
+    battery = psutil.sensors_battery()
+    percentage = battery.percent
+
+    if percentage == FULL_CHARGE:
+        notification.notify(title="Battery Notification", message="Charging full, time to unplug.", timeout=None)
+    if percentage > MAX_CHARGE_THRESHOLD:
+        notification.notify(title="Battery Notification", message="Charging is about to be full.", timeout=None)
+    elif percentage < MIN_BATTERY_THRESHOLD:
+        notification.notify(title="Battery Notification", message="Low battery, consider charging.", timeout=None)
+
+    # Sleep for 1 minute before checking again
+    sleep(60)
