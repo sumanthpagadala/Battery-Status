fc72d0d70b9576dfae8f05286565a2c8f3ebe243
sumanthpagadala <pagadala.sumanth2002@gmail.com>
2023-07-11 21:34:20 +0530

    update number two

--- battery.py
+++ battery.py
@@ -3,4 +3,9 @@ import psutil
 battery = psutil.sensors_battery()
 percentage = battery.percent
 
-print(f"Battery percentage: {percentage}%")
+if percentage > 95:
+    print("The charging is about to be full")
+elif percentage == 100:
+    print("Charging full, time to unplug")
+else:
+    print(f"Battery percentage: {percentage}%")
